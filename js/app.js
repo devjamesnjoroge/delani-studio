f0dbd29dd463113bfb2fade9595c7d0fab0d8929
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-03-10 16:18:06 +0300

    ft: add hover effect on portfolio

--- js/app.js
+++ js/app.js
@@ -24,4 +24,78 @@ $(document).ready(function () {
     $(this).css("display", "none");
     $(".product").css("display", "initial");
   });
+
+  //PORTFOLIO HOVER EFFECTS
+  $("#work1").mouseenter(function () {
+    $("#work1-overlay").css(
+      "clipPath",
+      "polygon(0 0, 100% 0, 100% 100%, 0% 100%)"
+    );
+  });
+  $("#work1").mouseleave(function () {
+    $("#work1-overlay").css("clipPath", "polygon(0 0, 0 0, 0 100%, 0% 100%)");
+  });
+  $("#work2").mouseenter(function () {
+    $("#work2-overlay").css(
+      "clipPath",
+      "polygon(0 0, 100% 0, 100% 100%, 0% 100%)"
+    );
+  });
+  $("#work2").mouseleave(function () {
+    $("#work2-overlay").css("clipPath", "polygon(0 0, 0 0, 0 100%, 0% 100%)");
+  });
+  $("#work3").mouseenter(function () {
+    $("#work3-overlay").css(
+      "clipPath",
+      "polygon(0 0, 100% 0, 100% 100%, 0% 100%)"
+    );
+  });
+  $("#work3").mouseleave(function () {
+    $("#work3-overlay").css("clipPath", "polygon(0 0, 0 0, 0 100%, 0% 100%)");
+  });
+  $("#work4").mouseenter(function () {
+    $("#work4-overlay").css(
+      "clipPath",
+      "polygon(0 0, 100% 0, 100% 100%, 0% 100%)"
+    );
+  });
+  $("#work4").mouseleave(function () {
+    $("#work4-overlay").css("clipPath", "polygon(0 0, 0 0, 0 100%, 0% 100%)");
+  });
+  $("#work5").mouseenter(function () {
+    $("#work5-overlay").css(
+      "clipPath",
+      "polygon(0 0, 100% 0, 100% 100%, 0% 100%)"
+    );
+  });
+  $("#work5").mouseleave(function () {
+    $("#work5-overlay").css("clipPath", "polygon(0 0, 0 0, 0 100%, 0% 100%)");
+  });
+  $("#work6").mouseenter(function () {
+    $("#work6-overlay").css(
+      "clipPath",
+      "polygon(0 0, 100% 0, 100% 100%, 0% 100%)"
+    );
+  });
+  $("#work6").mouseleave(function () {
+    $("#work6-overlay").css("clipPath", "polygon(0 0, 0 0, 0 100%, 0% 100%)");
+  });
+  $("#work7").mouseenter(function () {
+    $("#work7-overlay").css(
+      "clipPath",
+      "polygon(0 0, 100% 0, 100% 100%, 0% 100%)"
+    );
+  });
+  $("#work7").mouseleave(function () {
+    $("#work7-overlay").css("clipPath", "polygon(0 0, 0 0, 0 100%, 0% 100%)");
+  });
+  $("#work8").mouseenter(function () {
+    $("#work8-overlay").css(
+      "clipPath",
+      "polygon(0 0, 100% 0, 100% 100%, 0% 100%)"
+    );
+  });
+  $("#work8").mouseleave(function () {
+    $("#work8-overlay").css("clipPath", "polygon(0 0, 0 0, 0 100%, 0% 100%)");
+  });
 });
